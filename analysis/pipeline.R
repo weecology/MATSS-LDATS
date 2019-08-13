72eec08cb9b08a9cfac42e6f309de2e36be79769
Renata Diaz <diaz.renatam@gmail.com>
2019-08-13 07:57:24 -0400

    short plan

--- analysis/pipeline.R
+++ analysis/pipeline.R
@@ -20,16 +20,9 @@ if (FALSE)
 ## Clean and transform the data into the appropriate format
 datasets <- build_datasets_plan(include_retriever_data = T, include_bbs_data = T,bbs_subset = c(1:5))
 
-#datasets <- datasets[10:12, ]
+datasets <- datasets[10:12, ]
 
-notransients_datasets <- drake::drake_plan(
-    filtered = target(remove_transients(data, threshold = .3),
-                 transform = map(data = !!rlang::syms(datasets$target)))
-)
-
-datasets = rbind(datasets, notransients_datasets)
-
-analyses <- build_ldats_analyses_plan(datasets, max_topics = c(5, 16), nchangepoints = c(0:1), formulas = c("time", "intercept"), nseeds = 100)
+analyses <- build_ldats_analyses_plan(datasets, max_topics = c(5, 16), nchangepoints = c(0:1), formulas = c("time", "intercept"), nseeds = 2)
 
 summary_tables <- drake_plan(
     lda_result_summary = collect_lda_result_summary(lda_results = lda_results),
