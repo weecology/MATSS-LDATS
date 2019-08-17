0c1c3a4bf939e6e089a4db7339f7001b67949fe1
Renata Diaz <diaz.renatam@gmail.com>
2019-08-17 13:51:22 -0400

    set up for large run

--- analysis/pipeline.R
+++ analysis/pipeline.R
@@ -33,12 +33,12 @@ datasets <- rbind(datasets, portal_annual_dataset)
 
 # lda_plan <- build_lda_plan(datasets, n_topics = as.numeric(2:3), nseeds = 5)
 
-lda_plan <- build_lda_plan(datasets, n_topics = as.numeric(2:5), nseeds = 4)
+lda_plan <- build_lda_plan(datasets, n_topics = as.numeric(seq(2, 16, by = 2)), nseeds = 100)
 
 lda_names <- lda_plan$target[ which(!grepl(lda_plan$target, pattern = "_results"))]
 
 
-ts_plan <- build_ts_plan(ldamodels_names = lda_names, nchangepoints = c(0:1), formulas = c("time", "intercept"), ts_control = list(nit = 100))
+ts_plan <- build_ts_plan(ldamodels_names = lda_names, nchangepoints = c(0:1), formulas = c("time", "intercept"), ts_control = list(nit = 10000))
 
 reports <- drake_plan(
     full_like_report = rmarkdown::render(
