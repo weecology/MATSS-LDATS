c42fb255b3d2a1d9c8d60541e51e864f9bbbe53c
Renata Diaz <diaz.renatam@gmail.com>
2019-09-01 13:54:56 -0400

    add model_info tests to LDA tests

---
+++ tests/testthat/test-4-TS-eval.R
@@ -0,0 +1,32 @@
+# context("Check TS analysis function")
+# 
+# # load prepackaged data
+# load(here::here("analysis", "exclosures_data.Rds"))
+# 
+# 
+# test_that("TS AICc functions work without heldout data", {
+#     expect_error(one_lda <- run_LDA(data = portal_annual, n_topics = 2, seed = 2), NA)
+#     expect_error(one_ts <- run_TS(one_lda, formulas = c(~ year), nchangepoints = c(0,1),weighting = "proportional", control = list(nit = 100)), NA)
+#     expect_true(all(c("ts", "model_info") %in% names(one_ts)))
+#     expect_equal(length(one_ts$ts), 2)
+#     
+#     
+# 
+# })
+# test_that("run_TS function works with heldout data", {
+#     expect_error(data_seg <- subset_data(portal_annual, n_segs = nrow(portal_annual$abundance), sequential = T, buffer = 2, which_seg = 1), NA)
+#     expect_error(one_lda <- run_LDA(data_seg, n_topics = 2, seed = 2), NA)
+#     expect_error(one_ts <- run_TS(one_lda, formulas = c("intercept", "time"), nchangepoints = c(0,1),weighting = "proportional", control = list(nit = 100)), NA)
+#   
+#       expect_true(all(c("ts", "model_info") %in% names(one_ts)))
+#     expect_type(one_ts$model_info, "list")
+#     expect_equal(nrow(one_ts$model_info), length(one_ts$ts))
+#     expect_equal(length(one_ts$ts), 4)
+#     for(i in 1:4) {
+#         expect_true(names(one_ts$ts)[i] == one_ts$model_info$ts_model_name[i])
+#     }
+#     expect_true(all(c("TS_fit", "list") %in% class(one_ts$ts[[1]])))
+#     
+#     expect_true("testll" %in% colnames(one_ts$model_info))
+#     
+# })
